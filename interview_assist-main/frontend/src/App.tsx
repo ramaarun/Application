7819019e7fec23d2e0fd2526bf5e7d097d3b7e64
admin <chandanapriya076@gmail.com>
2026-06-03 13:07:32 +0530

    h

--- interview_assist-main/frontend/src/App.tsx
+++ interview_assist-main/frontend/src/App.tsx
@@ -22,7 +22,7 @@ import { Landing } from './pages/Landing';
 export function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter basename="/application">
+      <BrowserRouter basename="/Application">
         <Toaster position="top-right" />
         <Routes>
           <Route path="/" element={<Landing />} />
